171c475e53982bae2d76347cfb32d550038999d1
Aulov <auloff@zoho.eu>
2020-02-01 17:08:17 +0400

    complete movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float cameraSpeed;   
+    public Transform target;
+    public Vector3 offset; 
+    private void FixedUpdate() {
+        Vector3 newPosition = Vector3.Lerp(transform.position, target.position + offset, cameraSpeed * Time.fixedDeltaTime);
+        transform.position = newPosition;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float turnSpeed;
+    private Rigidbody playerRigidbody;
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 newMovement = transform.forward * vertical * movementSpeed * Time.fixedDeltaTime;
+        playerRigidbody.MovePosition(playerRigidbody.position + newMovement);
+
+        Quaternion newRotation = Quaternion.Euler(0f, horizontal * turnSpeed, 0f);
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * newRotation);
+    }
+}

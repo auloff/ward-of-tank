d1beb5342709e41451f6fc7485f8a63c39b776b7
Aulov <auloff@zoho.eu>
2020-02-05 00:52:55 +0400

    add base enemy and object pooling

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private ObjectPool instance;
+    private Dictionary<string, Queue<GameObject>> pools;
+
+    public ObjectPool Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = this;
+
+            return instance;
+        }
+    }
+
+    [Serializable]
+    public class PoolInfo
+    {
+        public string tagName;
+        public GameObject objectToPool;
+        public int poolSize;
+    }
+
+    public PoolInfo[] poolsInfo;
+
+    private void Awake()
+    {
+        pools = new Dictionary<string, Queue<GameObject>>();
+        foreach (PoolInfo poolInfo in poolsInfo)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < poolInfo.poolSize; i++)
+            {
+                GameObject obj = Instantiate(poolInfo.objectToPool);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            pools.Add(poolInfo.tagName, objectPool);
+        }
+    }
+}
